8fdc390958426fae38f938aa5b9ef27ff70e3c29
adimtheworst <adim.vort@gmail.com>
2022-05-13 13:37:43 +0300

    Third-Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+// import faker from 'faker';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
 
 return (
+    <div className='ui container comments'>
+        <CommentDetail author="Sam" time = "6:00" comment ="Suck" />
+        <CommentDetail author="Alex" time = "7:00" comment="My" />
+        <CommentDetail author="Jane" time = "8:00" comment="balls, Mr. Anderson!" />
+    </div>
 
 
-<div>Fuck there!</div>
 
 
-);};
+    );
+};
 
 
 ReactDOM.render(<App />, document.querySelector('#root'))
